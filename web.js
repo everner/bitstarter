3c918a7c56cf25ee39ed8d08a7be4d844d39b002
Eric Verner <ericjv1@yahoo.com>
2013-07-09 10:44:32 +0000

    Print from index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = fs.readFileSync("index.html");
+
+var msg = buf.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(msg);
 });
 
 var port = process.env.PORT || 5000;
